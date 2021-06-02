a063c7083e63aee2801df3df3f16ce5e1e1a4840
nikitarizh <nrigworks@gmail.com>
2021-06-03 02:37:30 +0300

    feat: mappers

---
+++ src/main/java/com/nikitarizh/testtask/mapper/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.nikitarizh.testtask.mapper;
+
+import com.nikitarizh.testtask.dto.product.ProductCreateDTO;
+import com.nikitarizh.testtask.dto.product.ProductFullDTO;
+import com.nikitarizh.testtask.entity.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ProductMapper {
+
+    ProductMapper PRODUCT_MAPPER = Mappers.getMapper(ProductMapper.class);
+
+    ProductFullDTO mapToFullDTO(Product product);
+
+    Product mapToEntity(ProductCreateDTO productFullDTO);
+}
---
+++ src/main/java/com/nikitarizh/testtask/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.nikitarizh.testtask.mapper;
+
+import com.nikitarizh.testtask.dto.user.UserCreateDTO;
+import com.nikitarizh.testtask.dto.user.UserFullDTO;
+import com.nikitarizh.testtask.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper USER_MAPPER = Mappers.getMapper(UserMapper.class);
+
+    UserFullDTO mapToFullDTO(User user);
+
+    User mapToEntity(UserCreateDTO userCreateDTO);
+}
