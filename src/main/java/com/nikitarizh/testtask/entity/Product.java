5096e7b3af97f16728e2406b1c1b7130ca7f2ce2
nikitarizh <nrigworks@gmail.com>
2021-06-03 01:04:51 +0300

    feat: DTO constructor

--- src/main/java/com/nikitarizh/testtask/entity/Product.java
+++ src/main/java/com/nikitarizh/testtask/entity/Product.java
@@ -1,5 +1,8 @@
 package com.nikitarizh.testtask.entity;
 
+import com.nikitarizh.testtask.dto.product.ProductCreateDTO;
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
@@ -7,7 +10,6 @@ import java.util.List;
 
 @Entity
 public class Product {
-
     @Id
     private Integer id;
 
@@ -18,4 +20,13 @@ public class Product {
 
     @ManyToMany (mappedBy = "orderedProducts")
     private List<User> orderedBy;
+
+    public Product(ProductCreateDTO productCreateDTO) {
+        this.description = productCreateDTO.getDescription();
+        this.tags = productCreateDTO.getTags();
+    }
+
+    public Product() {
+
+    }
 }
