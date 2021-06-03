97a7b8f9f6a51373d21f97b5ec1c1fef4158f910
nikitarizh <nrigworks@gmail.com>
2021-06-04 02:42:56 +0300

    refactor: rename userType -> type

--- src/main/java/com/nikitarizh/testtask/entity/User.java
+++ src/main/java/com/nikitarizh/testtask/entity/User.java
@@ -14,7 +14,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    private UserType userType;
+    private UserType type;
 
     private String nickname;
 
