d93c270b2828d5ff48f7609fae987ba28bf487c3
nikitarizh <nrigworks@gmail.com>
2021-06-04 01:51:30 +0300

    refactor: MailService

--- src/main/java/com/nikitarizh/testtask/service/impl/MailServiceImpl.java
+++ src/main/java/com/nikitarizh/testtask/service/impl/MailServiceImpl.java
@@ -7,13 +7,7 @@ import com.nikitarizh.testtask.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import java.io.File;
 
 @Service
 public class MailServiceImpl implements MailService {
