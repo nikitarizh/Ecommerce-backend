4990004a19fc90c928ebe2547d72741030e22e03
nikitarizh <nrigworks@gmail.com>
2021-06-04 01:51:45 +0300

    refactor: UserService

--- src/main/java/com/nikitarizh/testtask/service/impl/UserServiceImpl.java
+++ src/main/java/com/nikitarizh/testtask/service/impl/UserServiceImpl.java
@@ -4,19 +4,17 @@ import com.nikitarizh.testtask.dto.user.UserCreateDTO;
 import com.nikitarizh.testtask.dto.user.UserFullDTO;
 import com.nikitarizh.testtask.entity.User;
 import com.nikitarizh.testtask.exception.UserNotFoundException;
-import com.nikitarizh.testtask.mapper.UserMapper;
 import com.nikitarizh.testtask.repository.UserRepository;
 import com.nikitarizh.testtask.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 
 import static com.nikitarizh.testtask.mapper.UserMapper.USER_MAPPER;
 
 @Service
-@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
@@ -27,6 +25,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public UserFullDTO findById(Integer id) throws UserNotFoundException {
         Optional<User> result = userRepository.findById(id);
         if (result.isEmpty()) {
@@ -37,6 +36,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public UserFullDTO create(UserCreateDTO userCreateDTO) {
         User newUser = userRepository.save(USER_MAPPER.mapToEntity(userCreateDTO));
         return USER_MAPPER.mapToFullDTO(newUser);
