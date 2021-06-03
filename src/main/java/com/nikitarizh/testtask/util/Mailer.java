08ef3b5a73ceac95004f43193354c985318285e7
nikitarizh <nrigworks@gmail.com>
2021-06-03 16:42:00 +0300

    feat: JavaMailSender bean

---
+++ src/main/java/com/nikitarizh/testtask/util/Mailer.java
@@ -0,0 +1,35 @@
+package com.nikitarizh.testtask.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class Mailer {
+
+    @Value("${emailUser}")
+    private String login;
+    @Value("${emailPassword}")
+    private String password;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(login);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
