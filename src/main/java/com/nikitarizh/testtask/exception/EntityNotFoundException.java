e7503e828c107822de118bbe6fd436d22d74cf68
nikitarizh <nrigworks@gmail.com>
2021-06-04 23:30:30 +0300

    feat: universal constructor

--- src/main/java/com/nikitarizh/testtask/exception/EntityNotFoundException.java
+++ src/main/java/com/nikitarizh/testtask/exception/EntityNotFoundException.java
@@ -1,7 +1,12 @@
 package com.nikitarizh.testtask.exception;
 
 public class EntityNotFoundException extends RuntimeException {
+
     public EntityNotFoundException(String entity, Integer id) {
         super("Entity " + entity + " is not found by id " + id);
+    }
+
+    public EntityNotFoundException(String entity, String field, String value) {
+        super("Entity " + entity + " is not found by field " + field + " with value " + value);
     }
 }
