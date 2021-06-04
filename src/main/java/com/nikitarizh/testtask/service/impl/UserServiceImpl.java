cc893c1c8d837c0b888411432bcb737abc0d613b
nikitarizh <nrigworks@gmail.com>
2021-06-04 04:46:05 +0300

    feat: password hashing

--- src/main/java/com/nikitarizh/testtask/service/impl/UserServiceImpl.java
+++ src/main/java/com/nikitarizh/testtask/service/impl/UserServiceImpl.java
@@ -7,6 +7,8 @@ import com.nikitarizh.testtask.exception.UserNotFoundException;
 import com.nikitarizh.testtask.repository.UserRepository;
 import com.nikitarizh.testtask.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +21,9 @@ import static com.nikitarizh.testtask.mapper.UserMapper.USER_MAPPER;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+
+    @Value("bcryptRounds")
+    private final Integer rounds;
 
     @Override
     @Transactional(readOnly = true)
@@ -34,7 +39,12 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public UserFullDTO create(UserCreateDTO userCreateDTO) {
+
         User newUser = userRepository.save(USER_MAPPER.mapToEntity(userCreateDTO));
+
+        String hashedPassword = BCrypt.hashpw(userCreateDTO.getPassword(), BCrypt.gensalt(rounds));
+        newUser.setPassword(hashedPassword);
+
         return USER_MAPPER.mapToFullDTO(newUser);
     }
 }
