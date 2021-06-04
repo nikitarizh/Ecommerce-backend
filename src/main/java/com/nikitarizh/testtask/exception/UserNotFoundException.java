e5521f0ca796faf3ec0f77dfa81d3818ceec07e4
nikitarizh <nrigworks@gmail.com>
2021-06-05 02:17:36 +0300

    feat: universal constructor

--- src/main/java/com/nikitarizh/testtask/exception/UserNotFoundException.java
+++ src/main/java/com/nikitarizh/testtask/exception/UserNotFoundException.java
@@ -1,7 +1,12 @@
 package com.nikitarizh.testtask.exception;
 
 public class UserNotFoundException extends EntityNotFoundException {
+
     public UserNotFoundException(Integer id) {
         super("User", id);
+    }
+
+    public UserNotFoundException(String nickname) {
+        super("User", "nickname", nickname);
     }
 }
