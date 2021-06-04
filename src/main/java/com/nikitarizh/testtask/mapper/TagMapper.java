3e45658b41501348fbd3d11ca2ad341db14c611e
nikitarizh <nrigworks@gmail.com>
2021-06-04 04:25:08 +0300

    feat: mapToPreviewDTO method

--- src/main/java/com/nikitarizh/testtask/mapper/TagMapper.java
+++ src/main/java/com/nikitarizh/testtask/mapper/TagMapper.java
@@ -2,6 +2,7 @@ package com.nikitarizh.testtask.mapper;
 
 import com.nikitarizh.testtask.dto.tag.TagCreateDTO;
 import com.nikitarizh.testtask.dto.tag.TagFullDTO;
+import com.nikitarizh.testtask.dto.tag.TagPreviewDTO;
 import com.nikitarizh.testtask.entity.Tag;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -12,6 +13,8 @@ public interface TagMapper {
     TagMapper TAG_MAPPER = Mappers.getMapper(TagMapper.class);
 
     TagFullDTO mapToFullDTO(Tag tag);
+
+    TagPreviewDTO mapToPreviewDTO(Tag tag);
 
     Tag mapToEntity(TagCreateDTO tagFullDTO);
 }
