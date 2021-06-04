858891b8bff47f9b1c73ac7e5a9e79e26cac1f35
nikitarizh <nrigworks@gmail.com>
2021-06-04 11:13:06 +0300

    fix: @Repository annotation

--- src/main/java/com/nikitarizh/testtask/repository/TagRepository.java
+++ src/main/java/com/nikitarizh/testtask/repository/TagRepository.java
@@ -2,6 +2,8 @@ package com.nikitarizh.testtask.repository;
 
 import com.nikitarizh.testtask.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, Integer> {
 }
